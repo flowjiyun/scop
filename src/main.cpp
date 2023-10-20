df3eea3f5dfa7779ef9eb3028ccf45c63096e1ef
flowjiyun <gyp2795@gmail.com>
2023-10-20 15:21:11 +0900

    feat: 키 이벤트 콜백함수 등록
    - escape 키 입력시 윈도우 창 닫히도록 키 등록

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,12 @@ void OnFrameBufferSizeChange(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 }
 
+void OnKeyEvent(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
 int main(void) {
     std::cout << "hello word" << std::endl;
     if (!glfwInit()) {
@@ -44,7 +50,8 @@ int main(void) {
     // init viewport
     OnFrameBufferSizeChange(window, WINDOW_WIDTH, WINDOW_HEIGHT);
     glfwSetFramebufferSizeCallback(window, OnFrameBufferSizeChange);
-    
+    glfwSetKeyCallback(window, )
+
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents(); 
     }
