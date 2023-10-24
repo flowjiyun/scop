c250718b61dd15e27c581ba19b511cd479c140f9
flowjiyun <gyp2795@gmail.com>
2023-10-24 17:33:55 +0900

    feat: program 헤더 구현
    - shader 객체를 가지고 그래픽 파이프라인을 만들 수 있는데 이를 program이라는 객체로 만듬

---
+++ src/program.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "common.h"
+#include "shader.h"
+
+CLASS_PTR(Program)
+class Program {
+    public:
+        static ProgramUPtr Create(const std::vector<ShaderPtr>& shaders);
+        ~Program();
+        uint32_t Get() const {return m_programId;}
+    
+    private:
+        Program() {}
+        bool Link(const std::vector<ShaderPtr>& shaders);
+        uint32_t m_programId {0};
+};
+
