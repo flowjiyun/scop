8141cd527994246a4cf904fb59bb459f5c9dce3c
flowjiyun <gyp2795@gmail.com>
2023-10-28 16:05:11 +0900

    feat: 새로운 vertex arribute 적용
    - 정점 정보에 color값 적용하기

--- src/context.cpp
+++ src/context.cpp
@@ -10,33 +10,32 @@ ContextUPtr Context::Create() {
 
 void Context::Render() {
     glClear(GL_COLOR_BUFFER_BIT);
-    static float time = 0.0f;
-    float t = sinf(time) * 0.5f + 0.5f;
-    auto loc = glGetUniformLocation(m_program->Get(), "color");
-    m_program->Use();
-    glUniform4f(loc, t*t, 2.0f*t*(1.0f-t), (1.0f-t)*(1.0f-t), 1.0f);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-    time += 0.016f;
+    // static float time = 0.0f;
+    // float t = sinf(time) * 0.5f + 0.5f;
+    // auto loc = glGetUniformLocation(m_program->Get(), "color");
     // m_program->Use();
+    // glUniform4f(loc, t*t, 2.0f*t*(1.0f-t), (1.0f-t)*(1.0f-t), 1.0f);
     // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    // time += 0.016f;
+    m_program->Use();
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
 
 bool Context::Init() {
     float vertices[] = {
-    0.5f, 0.5f, 0.0f, // top right
-    0.5f, -0.5f, 0.0f, // bottom right
-    -0.5f, -0.5f, 0.0f, // bottom left
-    -0.5f, 0.5f, 0.0f, // top left
+        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // top right, red
+        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom right, green
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom left, blue
+        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, // top left, yellow
     };
 
     uint32_t indices[] = { // note that we start from 0!
-    0, 1, 3, // first triangle
-    1, 2, 3, // second triangle
+        0, 1, 3, // first triangle
+        1, 2, 3, // second triangle
     };
     //init shaders
-    ShaderPtr vertexShader = Shader::CreateFromFile("./shader/simple.vs", GL_VERTEX_SHADER);
-    ShaderPtr fragmentShader = Shader::CreateFromFile("./shader/simple.fs", GL_FRAGMENT_SHADER);
+    ShaderPtr vertexShader = Shader::CreateFromFile("./shader/color_vertex.vs", GL_VERTEX_SHADER);
+    ShaderPtr fragmentShader = Shader::CreateFromFile("./shader/color_vertex.fs", GL_FRAGMENT_SHADER);
     if (!vertexShader || !fragmentShader) {
         std::cout << "fail to init context" << std::endl;
         return false;
@@ -54,7 +53,8 @@ bool Context::Init() {
 
     m_vertexLayout = VertexLayout::Create();
     m_vertextBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, GL_STATIC_DRAW, vertices, sizeof(vertices));
-    m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+    m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
+    m_vertexLayout->SetAttrib(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, sizeof(float) * 3);
     m_indexBuffer = Buffer::CreateWithData(GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW, indices, sizeof(indices));
 
 
