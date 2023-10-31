53962e9c16c0f50ddee93f31ddf5d97d467f5d70
flowjiyun <gyp2795@gmail.com>
2023-10-31 17:19:01 +0900

    feat: 분산광 적용한 shader

--- shader/lighting.fs
+++ shader/lighting.fs
@@ -2,14 +2,19 @@
 
 in vec3 normal;
 in vec2 texCoord;
+in vec3 position;
 out vec4 fragColor;
 
 uniform vec3 lightColor;
 uniform vec3 objectColor;
 uniform float ambientStrength;
+uniform vec3 lightPosition;
 
 void main() {
     vec3 ambient = ambientStrength * lightColor;
-    vec3 result = ambient * objectColor;
+    vec3 lightDir = normalize(lightPos - position);
+    vec3 pixelNorm = normalize(normal);
+    vec3 diffuse = max(dot(lightDir, pixelNorm), 0.0) * lightColor;
+    vec3 result = (ambient + diffuse) * objectColor;
     fragColor = vec4(result, 1.0);
 }
